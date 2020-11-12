6377024b970a64fa406ec6bcd9b95094a23548b5
nandani2306 <nandanimadhukar@gmail.com>
2020-11-12 10:10:19 -0800

    changes in st within function

--- src/main/scala/cse512/SpatialQuery.scala
+++ src/main/scala/cse512/SpatialQuery.scala
@@ -1,49 +1,46 @@
+//Group 17 Phase 1
 package cse512
 
 import org.apache.spark.sql.SparkSession
-
 import scala.math.sqrt
 import scala.math.pow
 
 object SpatialQuery extends App{
-  def ST_Contains(queryRectangle:String, pointString:String):Boolean={
+def ST_Within(pointString1:String, pointString2:String, distance:Double): Boolean = {
+
+    if (pointString1 == null || pointString1.isEmpty() || pointString2 == null || pointString2.isEmpty() || distance <= 0.00)
+        return false //null checks
+	
+    val point1_coordinates = pointString1.split(",") //getting coordinates of first point
+    var point1x = point1_coordinates(0).toDouble
+    var point1y = point1_coordinates(1).toDouble
+
+    val point2_coordinates = pointString2.split(",") //getting coordinates of second point
+    var point2x = point2_coordinates(0).toDouble
+    var point2y = point2_coordinates(1).toDouble
+
+    // getting the distance between these points
+    var euc_dist = sqrt(pow(point1x - point2x, 2) + pow(point1y - point2y, 2))
+    if (euc_dist <= distance)
+        return true
+    else
+        return false
+	}
+def ST_Contains(queryRectangle:String, pointString:String):Boolean={
     val point = pointString.split(',').map(_.toDouble)
     val rectangle = queryRectangle.split(',').map(_.toDouble)
 
-    val high_bound_x = math.max(rectangle(0), rectangle(2))
-    val low_bound_x = math.min(rectangle(0), rectangle(2))
-    val high_bound_y = math.max(rectangle(1), rectangle(3))
-    val low_bound_y = math.min(rectangle(1), rectangle(3))
+    val upper_bound_x = math.max(rectangle(0), rectangle(2))
+    val lower_bound_x = math.min(rectangle(0), rectangle(2))
+    val upper_bound_y = math.max(rectangle(1), rectangle(3))
+    val lower_bound_y = math.min(rectangle(1), rectangle(3))
 
     val point_x = point(0)
     val point_y = point(1)
 
-    if (point_x > high_bound_x || point_x < low_bound_x || point_y > high_bound_y || point_y < low_bound_y) return false
+    if (point_x > upper_bound_x || point_x < lower_bound_x || point_y > upper_bound_y || point_y < lower_bound_y) return false
     else return true
   }
-
-  def ST_Within(pointString1:String, pointString2:String, distance:Double): Boolean = {
-    /*Checking if the inputs are empty/null or distance is zero, then it will return False*/
-
-    if (pointString1 == null || pointString1.isEmpty() || pointString2 == null || pointString2.isEmpty() || distance <= 0.00)
-      return false
-
-    val point1_coordinates = pointString1.split(",")
-    var p1x = point1_coordinates(0).toDouble
-    var p1y = point1_coordinates(1).toDouble
-
-    val point2_coordinates = pointString2.split(",")
-    var p2x = point2_coordinates(0).toDouble
-    var p2y = point2_coordinates(1).toDouble
-
-    // Distance between two points
-    var euc_dist = sqrt(pow(p1x - p2x, 2) + pow(p1y - p2y, 2))
-    if (euc_dist <= distance)
-      return true
-    else
-      return false
-  }
-
   def runRangeQuery(spark: SparkSession, arg1: String, arg2: String): Long = {
 
     val pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(arg1);
